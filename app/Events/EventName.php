cc6b2e72d37a0cf87af43d4cbf29e2b80d8b6e4d
dananjayarumesh <rumeshpriv@gmail.com>
2018-07-03 00:20:30 +0530

    first commit

---
+++ app/Events/EventName.php
@@ -0,0 +1,23 @@
+<?php namespace App\Events;
+
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+class EventName
+{
+    use SerializesModels;
+
+    public $data;
+
+    public function __construct()
+    {
+        $this->data = array(
+            'power'=> '10'
+        );
+    }
+
+    public function broadcastOn()
+    {
+        return ['test-channel'];
+    }
+}
